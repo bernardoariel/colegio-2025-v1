edf96d85c608d6fac64196b15e621313b5cdae57
Ariel Hernán Bernardo <arielbernardo@hotmail.com.ar>
2025-04-18 20:21:56 -0300

    agregando vista de errores en facturaElectronica.php

--- extensiones/fpdf/pdf/facturaElectronica.php
+++ extensiones/fpdf/pdf/facturaElectronica.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-error_reporting(0);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include('../fpdf.php');
 include ('../../barcode.php');
 
